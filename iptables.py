bc312a026e364656580cab8fddf76bd109cff358
Mahdi Aria <142175914+MahdiAriaa@users.noreply.github.com>
2023-12-26 14:41:32 +0330

    Update iptables.py

--- iptables.py
+++ iptables.py
@@ -18,9 +18,9 @@ panel_ip = input("Enter Panel IP : ")
 #tunnel_ip = sys.argv[1]
 #panel_ip = sys.argv[2]
 
-os.system("sysctl net.ipv4.ip_forward=1")
-os.system(f"iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {tunnel_ip}")
-os.system(f"iptables -t nat -A PREROUTING -j DNAT --to-destination {panel_ip}")
-os.system("iptables -t nat -A POSTROUTING -j MASQUERADE")
+os.system("sudo sysctl net.ipv4.ip_forward=1")
+os.system(f"sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j DNAT --to-destination {tunnel_ip}")
+os.system(f"sudo iptables -t nat -A PREROUTING -j DNAT --to-destination {panel_ip}")
+os.system("sudo iptables -t nat -A POSTROUTING -j MASQUERADE")
 
-os.system("iptables -L -n -t nat")
+os.system("sudo iptables -L -n -t nat")
